5b7d61cc8ff09cc1535581af8d387f0cb66f6e5e
Saara87 <c1lasa04@students.oamk.fi>
2022-10-14 11:23:02 +0300

    Valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+const URL = 'https://api.agify.io/?name=';
+const country = '&country_id=FI';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+ 
+  function handleSubmit(e) {
+    e.preventDefault();
+  
+    
+    fetch(URL + name + country)
+      .then(response => response.json())
+      .then (
+        (response) => {
+          setAge(response.age);
+        }, (error) => {
+          alert(error);
+        }
+      )
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '10px'}}>
+      <form onSubmit={handleSubmit}>
+        <h1>What is your age?</h1>
+        <p>Please write your name and let the algorithm predict your age. This is exciting!</p>
+        <div style={{margin: '10px'}}>
+          <label>Name</label>&nbsp;
+          <input type="text" placeholder="Write your name here"
+          value={name} onChange={e => setName(e.target.value)} />
+        </div>
+        <div style={{margin: '10px'}}>
+          <button>Press</button>
+        </div>
+        <div style={{margin: '10px'}}>
+          <output>Your age is&nbsp;{age}&nbsp;years.</output>
+        </div>
+      </form>
     </div>
   );
 }
